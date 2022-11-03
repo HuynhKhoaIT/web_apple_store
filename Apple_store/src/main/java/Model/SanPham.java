c46b2a938ca34175501326a4541eb6a8f5d48dd5
HuynhKhoaIT <114349590+HuynhKhoaIT@users.noreply.github.com>
2022-11-03 18:32:25 +0700

    Update SanPham.java

--- Apple_store/src/main/java/Model/SanPham.java
+++ Apple_store/src/main/java/Model/SanPham.java
@@ -9,101 +9,103 @@ public class SanPham {
     int GiaBanThuong;
     int GiaKhuyenMai;
     int SoLuong;
+    String Anh;
 
     public SanPham() {
     }
-
-    public SanPham(int maSP, int maDM, String tenSP, String moTa, int giaGoc, int giaBanThuong, int giaKhuyenMai, int soLuong) {
-        MaSP = maSP;
-        MaDM = maDM;
-        TenSP = tenSP;
-        MoTa = moTa;
-        GiaGoc = giaGoc;
-        GiaBanThuong = giaBanThuong;
-        GiaKhuyenMai = giaKhuyenMai;
-        SoLuong = soLuong;
-    }
-
-    public int getMaSP() {
-        return MaSP;
-    }
-
-    public void setMaSP(int maSP) {
-        MaSP = maSP;
-    }
-
-    public int getMaDM() {
-        return MaDM;
-    }
-
-    public void setMaDM(int maDM) {
-        MaDM = maDM;
-    }
-
-    public String getTenSP() {
-        return TenSP;
-    }
-
-    public void setTenSP(String tenSP) {
-        TenSP = tenSP;
-    }
-
-    public String getMoTa() {
-        return MoTa;
-    }
-
-    public void setMoTa(String moTa) {
-        MoTa = moTa;
-    }
-
-    public int getGiaGoc() {
-        return GiaGoc;
-    }
-
-    public void setGiaGoc(int giaGoc) {
-        GiaGoc = giaGoc;
-    }
-
-    public int getGiaBanThuong() {
-        return GiaBanThuong;
-    }
-
-    public void setGiaBanThuong(int giaBanThuong) {
-        GiaBanThuong = giaBanThuong;
-    }
-
-    public int getGiaKhuyenMai() {
-        return GiaKhuyenMai;
-    }
-
-    public void setGiaKhuyenMai(int giaKhuyenMai) {
-        GiaKhuyenMai = giaKhuyenMai;
-    }
-
-    public int getSoLuong() {
-        return SoLuong;
-    }
-
-    public void setSoLuong(int soLuong) {
-        SoLuong = soLuong;
-    }
     
+	public SanPham(int maSP, int maDM, String tenSP, String moTa, int giaGoc, int giaBanThuong, int giaKhuyenMai,
+			int soLuong, String anh) {
+		super();
+		MaSP = maSP;
+		MaDM = maDM;
+		TenSP = tenSP;
+		MoTa = moTa;
+		GiaGoc = giaGoc;
+		GiaBanThuong = giaBanThuong;
+		GiaKhuyenMai = giaKhuyenMai;
+		SoLuong = soLuong;
+		Anh = anh;
+	}
 
-    @Override
-    public String toString() {
-        return "SanPham{" +
-                "MaSP=" + MaSP +
-                ", MaDM=" + MaDM +
-                ", TenSP='" + TenSP + '\'' +
-                ", MoTa='" + MoTa + '\'' +
-                ", GiaGoc=" + GiaGoc +
-                ", GiaBanThuong=" + GiaBanThuong +
-                ", GiaKhuyenMai=" + GiaKhuyenMai +
-                ", SoLuong=" + SoLuong +
-                '}';
-    }
-//    public static void main(String[] args) {
-//		SanPham sp= new SanPham();
-//		System.out.println(sp.toString());
-//        }
+	public int getMaSP() {
+		return MaSP;
+	}
+
+	public void setMaSP(int maSP) {
+		MaSP = maSP;
+	}
+
+	public int getMaDM() {
+		return MaDM;
+	}
+
+	public void setMaDM(int maDM) {
+		MaDM = maDM;
+	}
+
+	public String getTenSP() {
+		return TenSP;
+	}
+
+	public void setTenSP(String tenSP) {
+		TenSP = tenSP;
+	}
+
+	public String getMoTa() {
+		return MoTa;
+	}
+
+	public void setMoTa(String moTa) {
+		MoTa = moTa;
+	}
+
+	public int getGiaGoc() {
+		return GiaGoc;
+	}
+
+	public void setGiaGoc(int giaGoc) {
+		GiaGoc = giaGoc;
+	}
+
+	public int getGiaBanThuong() {
+		return GiaBanThuong;
+	}
+
+	public void setGiaBanThuong(int giaBanThuong) {
+		GiaBanThuong = giaBanThuong;
+	}
+
+	public int getGiaKhuyenMai() {
+		return GiaKhuyenMai;
+	}
+
+	public void setGiaKhuyenMai(int giaKhuyenMai) {
+		GiaKhuyenMai = giaKhuyenMai;
+	}
+
+	public int getSoLuong() {
+		return SoLuong;
+	}
+
+	public void setSoLuong(int soLuong) {
+		SoLuong = soLuong;
+	}
+
+	public String getAnh() {
+		return Anh;
+	}
+
+	public void setAnh(String anh) {
+		Anh = anh;
+	}
+
+	@Override
+	public String toString() {
+		return "SanPham [MaSP=" + MaSP + ", MaDM=" + MaDM + ", TenSP=" + TenSP + ", MoTa=" + MoTa + ", GiaGoc=" + GiaGoc
+				+ ", GiaBanThuong=" + GiaBanThuong + ", GiaKhuyenMai=" + GiaKhuyenMai + ", SoLuong=" + SoLuong
+				+ ", Anh=" + Anh + "]";
+	}
+	
+    
 }
