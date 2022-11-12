3243c577b292831b251b67c64559fd6a9923b59c
MinhQuan21112002 <93760661+MinhQuan21112002@users.noreply.github.com>
2022-11-12 10:55:56 +0700

    Update TaiKhoanDAO.java

--- Apple_store/src/main/java/DAO/TaiKhoanDAO.java
+++ Apple_store/src/main/java/DAO/TaiKhoanDAO.java
@@ -1,5 +1,71 @@
 package DAO;
 
-public class TaiKhoanDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import Connection.ConnectJDBC;
+import Model.TaiKhoan;
+
+public class TaiKhoanDAO {
+	 Connection conn=null;
+	    PreparedStatement ps =null;
+	    ResultSet rs = null;
+	    public TaiKhoan getTaiKhoanByMaTK(int MaTK){
+			String query = "Select * From TaiKhoan where MaTK= ?";
+	        try {
+	            conn =new ConnectJDBC().getConnection();
+	            ps =conn.prepareStatement(query);
+	            ps.setInt(1,MaTK);
+	            rs =ps.executeQuery();
+	            while(rs.next())
+	            {
+	                return new TaiKhoan(rs.getInt(1),rs.getInt(2),rs.getString(3),rs.getString(4),rs.getString(5));
+	            }
+	        }
+	        catch (Exception e) {
+	        }
+	        return null;
+
+	    }
+	    
+	    public TaiKhoan checkAccount(int id, String oldPass) {
+			System.out.println(id);
+			System.out.println(oldPass);
+			try {
+				conn = new ConnectJDBC().getConnection();
+
+				ps = conn.prepareStatement("select * from TaiKhoan where MaTK= ? and MatKhau=?");
+				ps.setInt(1, id);
+				ps.setString(2, oldPass);
+				rs = ps.executeQuery();
+
+				while (rs.next()) {
+					TaiKhoan a = new TaiKhoan(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4), rs.getString(5));
+
+					return a;
+				}
+
+			} catch (Exception e) {
+				System.out.println("ket noi that bai");
+			}
+			return null;
+		}
+	    
+	    
+	    public void ChangePassWord(int id, String newPass) {
+
+			String querry = "update TaiKhoan set MatKhau=? where MaTK=?";
+			try {
+
+				conn = new ConnectJDBC().getConnection();
+
+				ps = conn.prepareStatement(querry);
+				ps.setString(1, newPass);
+				ps.setInt(2, id);
+				ps.executeUpdate();
+
+			} catch (Exception e) {
+			}
+		}
 }
