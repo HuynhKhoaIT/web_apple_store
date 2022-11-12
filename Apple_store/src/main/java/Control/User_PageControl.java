9eb583dc1d5915432b8d090fa7d7835c10e387b5
MinhQuan21112002 <93760661+MinhQuan21112002@users.noreply.github.com>
2022-11-12 10:48:04 +0700

    Add files via upload

---
+++ Apple_store/src/main/java/Control/User_PageControl.java
@@ -0,0 +1,25 @@
+package Control;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserPageControl
+ */
+@WebServlet(name = "User_PageControl", value = "/UserPage")
+public class User_PageControl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/shop/user.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int MaTK=Integer.parseInt(request.getParameter("maTK"));
+	}
+
+}
