09b1a05e9a53fd791709925b51876830ce8da7eb
MinhQuan21112002 <93760661+MinhQuan21112002@users.noreply.github.com>
2022-11-18 23:29:28 +0700

    PayPal

---
+++ Apple_store/src/main/java/Control/ExecutePaymentServlet.java
@@ -0,0 +1,86 @@
+package Control;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+import com.paypal.api.payments.*;
+import com.paypal.base.rest.PayPalRESTException;
+import DAO.*;
+import Model.Cart;
+import Model.KhachHang;
+import Model.SanPham;
+
+@WebServlet("/execute_payment")
+public class ExecutePaymentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public ExecutePaymentServlet() {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		
+		response.setContentType("text/html; charset=UTF-8");
+		String paymentId = request.getParameter("paymentId");
+		String payerId = request.getParameter("PayerID");
+
+		String name_raw = request.getParameter("name");
+		String phone_raw = request.getParameter("phone");
+		String address_raw = request.getParameter("address");
+		System.out.println(name_raw);
+		try {
+			PaymentServices paymentServices = new PaymentServices();
+			Payment payment = paymentServices.executePayment(paymentId, payerId);
+
+			PayerInfo payerInfo = payment.getPayer().getPayerInfo();
+			Transaction transaction = payment.getTransactions().get(0);
+
+			request.setAttribute("payer", payerInfo);
+			request.setAttribute("transaction", transaction);
+
+//		        HttpSession session = request.getSession();
+//		        KhachHang khachHang = (KhachHang) session.getAttribute("account");
+
+			SanPhamDAO dao = new SanPhamDAO();
+			List<SanPham> list = dao.getAll();
+			Cookie[] arr = request.getCookies();
+			String txt = "";
+			if (arr != null) {
+				for (Cookie o : arr) {
+					if (o.getName().equals("cart")) {
+						txt += o.getValue();
+					}
+				}
+			}
+			Cart cart = new Cart(txt, list);
+//	        HttpSession session = request.getSession();
+//	        KhachHang khachHang = (KhachHang) session.getAttribute("account");
+
+			KhachHangDAO khachHangDAO = new KhachHangDAO();
+
+			if (khachHangDAO.getKhachHangByPhone(phone_raw) == null) {
+				khachHangDAO.addKhachHang(name_raw, address_raw, phone_raw);
+			}
+			KhachHang khachHang = khachHangDAO.getKhachHangByPhone(phone_raw);
+
+			DonHangDAO donHangDAO = new DonHangDAO();
+			donHangDAO.addOrder(khachHang, cart);
+			Cookie c = new Cookie("cart", "");
+			c.setMaxAge(0);
+			response.addCookie(c);
+			response.sendRedirect("show");
+
+		} catch (PayPalRESTException ex) {
+			request.setAttribute("errorMessage", ex.getMessage());
+			ex.printStackTrace();
+			request.getRequestDispatcher("error.jsp").forward(request, response);
+		}
+	}
+
+}
